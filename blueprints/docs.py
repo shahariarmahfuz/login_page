49648c64f56fa217f791325c0fd12560e39e5ba8
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-09-17 07:07:12 +0600

    Update docs.py

--- blueprints/docs.py
+++ blueprints/docs.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 docs_blueprint = Blueprint('docs', __name__, template_folder='../templates')
 
-@profile_blueprint.route('/docs')
+@docs_blueprint.route('/docs')
 def docs():
     return render_template('docs.html')

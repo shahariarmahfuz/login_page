906e11afea2dd9c4bff2ed6bb639b4338e926e25
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-09-19 12:14:59 +0600

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,5 +1,5 @@
 let sound = new Howl({
-    src: ["https://files.catbox.moe/il9cy7.mp3"],
+    src: ["https://files.catbox.moe/t5yntb.mp3"],
     autoplay: true,
     loop: true,
     format: ['mp3'],

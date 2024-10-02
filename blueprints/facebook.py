d148e24dca3d50679c1769213564bdf4e4cee2e0
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-10-02 14:29:23 +0600

    Update facebook.py

--- blueprints/facebook.py
+++ blueprints/facebook.py
@@ -1,30 +1,31 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request
 import requests
 
 facebook_blueprint = Blueprint('facebook', __name__, template_folder='templates')
 
-@facebook_blueprint.route('/Facebook', methods=['GET'])
+@facebook_blueprint.route('/facebook', methods=['GET'])
 def facebook():
     return render_template('Facebook.html', result=None)
 
 @facebook_blueprint.route('/download', methods=['GET'])
 def download():
-    video_url = request.args.get('url')
-    if not video_url:
-        return redirect('/facebook')
+    result = None
+    video_url = request.args.get('url')  # Get the URL from the query parameters
 
-    api_url = f"https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php?url={video_url}"
-
-    headers = {
-        'x-rapidapi-host': 'facebook-reel-and-video-downloader.p.rapidapi.com',
-        'x-rapidapi-key': '80f0a59b76msh49bd109c53bf5a9p1c1366jsn57f6e67af29d'  # Replace with your actual API key
-    }
-
-    response = requests.get(api_url, headers=headers)
-
-    if response.status_code == 200:
-        result = response.json()  # assuming the API response is in JSON format
-    else:
-        result = {"error": "Error fetching data from API."}
+    if video_url:
+        api_url = f"https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php?url={video_url}"
+        
+        headers = {
+            'x-rapidapi-host': 'facebook-reel-and-video-downloader.p.rapidapi.com',
+            'x-rapidapi-key': '80f0a59b76msh49bd109c53bf5a9p1c1366jsn57f6e67af29d'
+        }
+        
+        response = requests.get(api_url, headers=headers)
+        
+        if response.status_code == 200:
+            result = response.json()  # assuming the API response is in JSON format
+        else:
+            result = "Error fetching data from API."
+    
 
     return render_template('Facebook.html', result=result)
